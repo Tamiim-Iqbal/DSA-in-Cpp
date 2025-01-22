019b3d695cb96474333a4780c9e18b3cef18a279
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2025-01-23 00:13:37 +0600

    Header File

---
+++ 01. Cpp/06. HeaderFile/01.HeaderFile.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>    //include all header file
+using namespace std;
+int main()
+{
+    // to deal with large input/output
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	cout<<"Hello";
+}
