30442dc4f7cb92acc6c274b7aea12d09edaab184
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2025-01-23 00:11:29 +0600

    Strings

---
+++ 01. Cpp/05. Strings/01.InputStrings.cpp
@@ -0,0 +1,19 @@
+//Loop
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    char ar[n];
+    for (int i=0; i<n; i++)
+    {
+        cin>>ar[i];
+    }
+    // for (int i=0; i<n; i++)
+    // {
+    //     cout<<ar[i]<<endl;
+    // }
+    cout<<ar<<endl;
+    return 0;
+}
---
+++ 01. Cpp/05. Strings/02.WithoutSpace.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char s[100];      
+    cin>>s;      // "cin" stop taking input if it finds space 
+    cout<<s; 
+}
